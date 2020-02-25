f77417f5f0056147bf5baabe782a8b498e336590
JAco9307 <skrivtiljacob@hotmail.com>
2020-02-25 13:32:09 +0100

    Added c++ project for rsa test

---
+++ project_1/workspace/test/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters_ps.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_cache.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_types.h \
+ ../src/platform_config.h
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_cache.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_types.h:
+
+../src/platform_config.h:
---
+++ project_2/test/Debug/src/helloworld.d
@@ -0,0 +1,54 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_types.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters_ps.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/bspconfig.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xgpio.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_assert.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xstatus.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xgpio_l.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_io.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xpseudo_asm.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xreg_cortexa9.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xpseudo_asm_gcc.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/sleep.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/bspconfig.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xgpio.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_assert.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xstatus.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xgpio_l.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_io.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xpseudo_asm.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xreg_cortexa9.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xpseudo_asm_gcc.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/skriv/workspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/sleep.h:
